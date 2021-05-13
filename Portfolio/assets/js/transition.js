49d443cdbf796c63eba1b5a329741a8f21daca11
Dylan <dgrant160@west-mec.org>
2021-05-13 15:17:39 -0700

    fixed transitions and added css animation

--- Portfolio/assets/js/transition.js
+++ Portfolio/assets/js/transition.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
+          scrollTop: (target.offset().top - 20)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -27,15 +27,15 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
+  // var navbarCollapse = function() {
+  //   if ($("#mainNav").offset().top > 100) {
+  //     $("#mainNav").addClass("navbar-shrink");
+  //   } else {
+  //     $("#mainNav").removeClass("navbar-shrink");
+  //   }
+  // };
   // Collapse now if page is not at top
-  navbarCollapse();
+  //navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
